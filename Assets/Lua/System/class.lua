0ccb51f04797e10510e37c9d60c4bab56207446e
QiuBuZai <wuyangyang228@qq.com>
2016-02-11 02:47:48 +0800

    版本更新

--- Assets/Lua/System/class.lua
+++ Assets/Lua/System/class.lua
@@ -18,61 +18,29 @@ function clone(object)
 end
 
 --Create an class.
-function class(classname, super)
-    local superType = type(super)
-    local cls
+function class(className, superName, superModulePath) 
+  
+  if superName ~= nil then
+    require(superModulePath)
+  end
+  
+  if _G[className] ~= nil then
+    return
+  end
+  
+  _G[className] = {}
+  _G[className].className = className
+    
+  if _G[superName] ~= nil then      
+    setmetatable(_G[className], _G[superName])
+    _G[superName].__index = _G[superName]    
+  end  
+  
+   _G[className].New = function(o)
+    local o = o or {}
+    setmetatable(o, _G[className])
+    _G[className].__index = _G[className]
+    return o
+  end
 
-    if superType ~= "function" and superType ~= "table" then
-        superType = nil
-        super = nil
-    end
-
-    if superType == "function" or (super and super.__ctype == 1) then
-        -- inherited from native C++ Object
-        cls = {}
-
-        if superType == "table" then
-            -- copy fields from super
-            for k,v in pairs(super) do cls[k] = v end
-            cls.__create = super.__create
-            cls.super    = super
-        else
-            cls.__create = super
-        end
-
-        cls.ctor    = function() end
-        cls.__cname = classname
-        cls.__ctype = 1
-
-        function cls.New(...)
-            local instance = cls.__create(...)
-            -- copy fields from class to native object
-            for k,v in pairs(cls) do instance[k] = v end
-            instance.class = cls
-            instance:ctor(...)
-            return instance
-        end
-
-    else
-        -- inherited from Lua Object
-        if super then
-            cls = clone(super)
-            cls.super = super
-        else
-            cls = {ctor = function() end}
-        end
-
-        cls.__cname = classname
-        cls.__ctype = 2 -- lua
-        cls.__index = cls
-
-        function cls.New(...)
-            local instance = setmetatable({}, cls)
-            instance.class = cls
-            instance:ctor(...)
-            return instance
-        end
-    end
-
-    return cls
 end
