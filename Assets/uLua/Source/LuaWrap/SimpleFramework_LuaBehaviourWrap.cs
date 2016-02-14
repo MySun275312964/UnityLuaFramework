cf96a1859623c8a84000e05f5e404f9b40582de2
QiuBuZai <wuyangyang228@qq.com>
2016-02-15 00:04:57 +0800

    wrap更新

--- Assets/uLua/Source/LuaWrap/SimpleFramework_LuaBehaviourWrap.cs
+++ Assets/uLua/Source/LuaWrap/SimpleFramework_LuaBehaviourWrap.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using LuaInterface;
 using Object = UnityEngine.Object;
@@ -18,10 +19,11 @@ public class SimpleFramework_LuaBehaviourWrap
 		{
 			new LuaField("modulePath", get_modulePath, set_modulePath),
 			new LuaField("luaClassName", get_luaClassName, set_luaClassName),
+			new LuaField("transforms", get_transforms, set_transforms),
 			new LuaField("luaInstance", get_luaInstance, set_luaInstance),
 		};
 
-		LuaScriptMgr.RegisterLib(L, "SimpleFramework.LuaBehaviour", typeof(SimpleFramework.LuaBehaviour), regs, fields, typeof(View));
+		LuaScriptMgr.RegisterLib(L, "SimpleFramework.LuaBehaviour", typeof(SimpleFramework.LuaBehaviour), regs, fields, typeof(UnityEngine.Behaviour));
 	}
 
 	[MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
@@ -85,6 +87,30 @@ public class SimpleFramework_LuaBehaviourWrap
 		}
 
 		LuaScriptMgr.Push(L, obj.luaClassName);
+		return 1;
+	}
+
+	[MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+	static int get_transforms(IntPtr L)
+	{
+		object o = LuaScriptMgr.GetLuaObject(L, 1);
+		SimpleFramework.LuaBehaviour obj = (SimpleFramework.LuaBehaviour)o;
+
+		if (obj == null)
+		{
+			LuaTypes types = LuaDLL.lua_type(L, 1);
+
+			if (types == LuaTypes.LUA_TTABLE)
+			{
+				LuaDLL.luaL_error(L, "unknown member name transforms");
+			}
+			else
+			{
+				LuaDLL.luaL_error(L, "attempt to index transforms on a nil value");
+			}
+		}
+
+		LuaScriptMgr.PushObject(L, obj.transforms);
 		return 1;
 	}
 
@@ -157,6 +183,30 @@ public class SimpleFramework_LuaBehaviourWrap
 		}
 
 		obj.luaClassName = LuaScriptMgr.GetString(L, 3);
+		return 0;
+	}
+
+	[MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+	static int set_transforms(IntPtr L)
+	{
+		object o = LuaScriptMgr.GetLuaObject(L, 1);
+		SimpleFramework.LuaBehaviour obj = (SimpleFramework.LuaBehaviour)o;
+
+		if (obj == null)
+		{
+			LuaTypes types = LuaDLL.lua_type(L, 1);
+
+			if (types == LuaTypes.LUA_TTABLE)
+			{
+				LuaDLL.luaL_error(L, "unknown member name transforms");
+			}
+			else
+			{
+				LuaDLL.luaL_error(L, "attempt to index transforms on a nil value");
+			}
+		}
+
+		obj.transforms = (List<Transform>)LuaScriptMgr.GetNetObject(L, 3, typeof(List<Transform>));
 		return 0;
 	}
 
