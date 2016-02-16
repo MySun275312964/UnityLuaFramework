957f7279127aeaf81851b42d883ffa39fcdca2ab
yangyang.wu <yangyang.wu@palmjoi.com>
2016-02-16 11:32:53 +0800

    更新用例

--- Assets/Lua/View/Test1.lua
+++ Assets/Lua/View/Test1.lua
@@ -10,6 +10,7 @@ end
 
 function Test1:Start()
   self.btn1.onClick:AddListener(Click1)
+  MessageCenter:GetInstance():SendMessage(1, 11, nil, Test1.Callback, self, "Test1 sended")
 end
 
 function Click1()
@@ -19,5 +20,9 @@ end
 
 function Test1:OnMessage(head, content)
   
+end
+
+function Test1:Callback(backMsg)
+  print(backMsg)
 end
 
