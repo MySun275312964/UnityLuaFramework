06de2099c86e92ee765a2b9416ac3b1e029a8faf
QiuBuZai <wuyangyang228@qq.com>
2016-05-16 15:14:48 +0800

    111

--- Assets/uLua/Source/Base/DelegateFactory.cs
+++ Assets/uLua/Source/Base/DelegateFactory.cs
@@ -17,11 +17,11 @@ public static class DelegateFactory
 		dict.Add(typeof(System.Reflection.MemberFilter), new DelegateValue(System_Reflection_MemberFilter));
 		dict.Add(typeof(System.Reflection.TypeFilter), new DelegateValue(System_Reflection_TypeFilter));
 		dict.Add(typeof(RectTransform.ReapplyDrivenProperties), new DelegateValue(RectTransform_ReapplyDrivenProperties));
-		dict.Add(typeof(TestLuaDelegate.VoidDelegate), new DelegateValue(TestLuaDelegate_VoidDelegate));
 		dict.Add(typeof(Camera.CameraCallback), new DelegateValue(Camera_CameraCallback));
 		dict.Add(typeof(AudioClip.PCMReaderCallback), new DelegateValue(AudioClip_PCMReaderCallback));
 		dict.Add(typeof(AudioClip.PCMSetPositionCallback), new DelegateValue(AudioClip_PCMSetPositionCallback));
 		dict.Add(typeof(Application.LogCallback), new DelegateValue(Application_LogCallback));
+		dict.Add(typeof(Application.AdvertisingIdentifierCallback), new DelegateValue(Application_AdvertisingIdentifierCallback));
 	}
 
 	[NoToLuaAttribute]
@@ -113,19 +113,6 @@ public static class DelegateFactory
 		return d;
 	}
 
-	public static Delegate TestLuaDelegate_VoidDelegate(LuaFunction func)
-	{
-		TestLuaDelegate.VoidDelegate d = (param0) =>
-		{
-			int top = func.BeginPCall();
-			IntPtr L = func.GetLuaState();
-			LuaScriptMgr.Push(L, param0);
-			func.PCall(top, 1);
-			func.EndPCall(top);
-		};
-		return d;
-	}
-
 	public static Delegate Camera_CameraCallback(LuaFunction func)
 	{
 		Camera.CameraCallback d = (param0) =>
@@ -168,6 +155,21 @@ public static class DelegateFactory
 	public static Delegate Application_LogCallback(LuaFunction func)
 	{
 		Application.LogCallback d = (param0, param1, param2) =>
+		{
+			int top = func.BeginPCall();
+			IntPtr L = func.GetLuaState();
+			LuaScriptMgr.Push(L, param0);
+			LuaScriptMgr.Push(L, param1);
+			LuaScriptMgr.Push(L, param2);
+			func.PCall(top, 3);
+			func.EndPCall(top);
+		};
+		return d;
+	}
+
+	public static Delegate Application_AdvertisingIdentifierCallback(LuaFunction func)
+	{
+		Application.AdvertisingIdentifierCallback d = (param0, param1, param2) =>
 		{
 			int top = func.BeginPCall();
 			IntPtr L = func.GetLuaState();
